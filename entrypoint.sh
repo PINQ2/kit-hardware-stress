1c1b38fe89ce4ea1308ba79807ebfa7ebb3ed1c9
Patrick <patrick.duboucher@pinq2.com>
2026-08-31 08:34:36 -0400

    Fix syntax error in stress arguments assignment

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ STRESS_ARGS=""
 if [ -n "$CPU" ]; then STRESS_ARGS="$STRESS_ARGS --cpu $CPU"; fi
 if [ -n "$RAM_WORKERS" ] && [ -n "$RAM_BYTES" ]; then STRESS_ARGS="$STRESS_ARGS --vm $RAM_WORKERS --vm-bytes $RAM_BYTES"; fi
 if [ -n "$TIME" ]; then STRESS_ARGS="$STRESS_ARGS --timeout $TIME"; fi
-STRESS_ARGS="$STRESS_ARGS --temp-path $OUT_PATH"; fi
+STRESS_ARGS="$STRESS_ARGS --temp-path $OUT_PATH";
 
 if [ -n "$STRESS_ARGS" ]; then
     echo "Starting CPU/RAM stress with args: $STRESS_ARGS"
